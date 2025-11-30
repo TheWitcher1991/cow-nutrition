1290ddb76f7dedb8518e1383bf5522d4aa50d5ff
Ashot Svazyan <ashot.svazyan222@gmail.com>
2025-11-30 12:47:09 +0300

    init

--- src/api.js
+++ src/api.js
@@ -5,6 +5,41 @@
 console.timeEnd('Launch')
 
 module.exports = (() => {
-    'use strict'
-    
+	'use strict'
+
+	const countR = D => Math.round(D / 2)
+
+	const countS = R => Math.PI * Math.pow(R, 2)
+
+	const L = 100
+
+	const Hay = 0.75
+
+	const countHayIndicators = (D, H, P) => {
+		R = countR(D)
+
+		S = countS(R)
+
+		const hayForHour = (Hay * L * 6) / 100
+
+		const hayWeight = Hay * (10 * 60 * H)
+
+		const hayPrice = hayWeight * P
+
+		return {
+			hayForHour,
+			hayWeight,
+			hayPrice,
+		}
+	}
+
+	document
+		.querySelector('.apply__cow')
+		.addEventListener('click', function () {
+			const D = +document.querySelector('#D').value
+			const H = +document.querySelector('#H').value
+			const P = +document.querySelector('#P').value
+
+			const indicators = countHayIndicators(D, H, P)
+		})
 })(0)
